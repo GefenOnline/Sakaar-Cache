e4360c9da963c7cbf62122d747705df26335905c
udiudi <udi@sheerci.com>
2017-11-16 15:42:49 +0200

    Initialized the cache manager with Redis config

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import cacheManager from 'cache-manager';
+import redisStore from 'cache-manager-ioredis';
+
+const redisUrl = process.env.REDIS_TEST_URL || process.env.REDIS_URL || `redis://:@localhost:6379`;
+const parts = redisUrl.split(':');
+const passAndhost = parts[2].split('@');
+
+const redisConfig = {
+    store: redisStore,
+    host: passAndhost[1],
+    port: parseInt(parts[3], 10),
+};
+
+if (process.env.NODE_ENV === 'test') {
+    redisConfig.db = 7;
+}
+
+if (_.size(passAndhost[0])) {
+    redisConfig.password = passAndhost[0];
+}
+
+const gefenCache = cacheManager.caching(redisConfig);
+
+const gefenCacheClient = gefenCache.store.getClient();
+
+gefenCacheClient.on('error', (error) => {
+    console.error(error);
+});
+
+export default gefenCache;
